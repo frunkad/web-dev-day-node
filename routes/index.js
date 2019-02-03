753246e2c0a61351138b825200e844fcb0dfb7a7
Darshan Baid <darshanbaid22@gmail.com>
2019-02-03 13:58:19 +0530

    added event view and addition

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,12 @@ const registrationsController = require('../controllers/registrations_controller
 
 // [TODO 1] create a route for displaying the home page of events (it'll contain the form to create an event and list of events)
 
+router.get('/', eventsController.home);
+
+
 
 // [TODO 2] create a route for receiving the data from new event form and send it to an action in the eventsController
+router.post('/events/create', eventsController.createEvent);
 
 
 // [TODO 3] create a route for api of events list and map it to corresponding action from eventsController
